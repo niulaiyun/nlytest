22e0a6e0fb33ce38662474a4842614b2549c5a2c
nilaiyun <aniulaiyunz@163.com>
2020-01-10 16:04:50 +0800

    新的

---
+++ src/main/java/com/nly/T.java
@@ -0,0 +1,11 @@
+package com.nly;
+/**
+ * @Author: Niu Lai Yun
+ * @Description: 
+ * @Date 2020年1月10日 下午4:04:11
+ */
+public class T {
+  public static void main(String[] args) {
+	System.out.println("新的东西");
+}
+}
